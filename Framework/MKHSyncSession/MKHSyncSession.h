26e988a361e2e622cd4ca52421e78231009a3386
maximkhatskevich <maxim@khatskevi.ch>
2016-03-23 21:54:02 -0400

    More methods support + mnor refactoring

---
+++ Framework/MKHSyncSession/MKHSyncSession.h
@@ -0,0 +1,19 @@
+//
+//  MKHSyncSession.h
+//  MKHSyncSession
+//
+//  Created by Maxim Khatskevich on 3/23/16.
+//  Copyright © 2016 Maxim Khatskevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKHSyncSession.
+FOUNDATION_EXPORT double MKHSyncSessionVersionNumber;
+
+//! Project version string for MKHSyncSession.
+FOUNDATION_EXPORT const unsigned char MKHSyncSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKHSyncSession/PublicHeader.h>
+
+
